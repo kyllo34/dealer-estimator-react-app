673a07aa820132e9651923743dac9c7ec5b2cc6f
Andrew Kyllo <kyllo34@gmail.com>
2020-05-04 09:03:02 -0700

    added boot-strap customer name form

--- src/components/CustomerBar/index.js
+++ src/components/CustomerBar/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './CustomerBar.scss';
+import { useForm } from 'react-hook-form'
+import { Form, Col, Button } from 'react-bootstrap';
 
 const CustomerBar = () => {
   return (
     <div className="Customer-Bar">
       <div className="customer-info">Customer Info:</div>
+      <Form>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formGridFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter First Name" />
+          </Form.Group>
+          <Form.Group as={Col} controlId="formGridLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter Last Name" />
+          </Form.Group>
+        </Form.Row>
+        
+      </Form>
     </div>
   )
 }
