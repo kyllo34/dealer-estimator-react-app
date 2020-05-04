270173df70f7f354c488fd8aa47038ad94a7b4ea
Andrew Kyllo <kyllo34@gmail.com>
2020-05-04 10:20:45 -0700

    finished customer form scaffold

--- src/components/CustomerBar/index.js
+++ src/components/CustomerBar/index.js
@@ -3,6 +3,8 @@ import './CustomerBar.scss';
 import { useForm } from 'react-hook-form'
 import { Form, Col, Button } from 'react-bootstrap';
 
+let states = [ "AK","AL","AR","AS","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","PR","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"]
+
 const CustomerBar = () => {
   return (
     <div className="Customer-Bar">
@@ -18,7 +20,38 @@ const CustomerBar = () => {
             <Form.Control type="text" placeholder="Enter Last Name" />
           </Form.Group>
         </Form.Row>
-        
+
+        <Form.Group controlId="formGridAddress1">
+          <Form.Label>Address</Form.Label>
+          <Form.Control placeholder="1234 Main St" />
+        </Form.Group>
+        <Form.Group controlId="formGridAddress2">
+          <Form.Label>Address 2</Form.Label>
+          <Form.Control placeholder="Apartment, studio, or floor" />
+        </Form.Group>
+
+        <Form.Row>
+          <Form.Group as={Col} controlId="formGridCity">
+            <Form.Label>City</Form.Label>
+            <Form.Control />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Label>State</Form.Label>
+            <Form.Control as="select" >
+              <option>Select...</option>
+              {states.map(state =>(
+                <option key={state} value={state}>{state}</option>
+              ))
+              }
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridZip">
+            <Form.Label>Zip</Form.Label>
+            <Form.Control />
+          </Form.Group>
+        </Form.Row>
       </Form>
     </div>
   )
